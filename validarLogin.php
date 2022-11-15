a78b4728de7702accf7258723dda7892c4853956
jordan07espi <jeespinosa5@espe.edu.ec>
2022-11-14 22:53:25 -0500

    login inicial

---
+++ validarLogin.php
@@ -0,0 +1,39 @@
+<?php
+$user=$_POST ['usuario'];
+$pass=$_POST['contraseña'];
+
+if(isset($user)){
+    //proceso de conexion a la base de datos
+    
+    //varable de conexion
+    $servername= "localhost";
+    $username= "root";
+    $password= "";
+    $dbname= "rentacar";
+    
+    //crear una conexion a la base de datos
+    $con = mysqli_connect($servername, $username, $password, $dbname) or die ("Error en la conexión");
+    //consultar si los datos son los que estan en la base
+    $consulta= "SELECT*FROM usuarios WHERE nombreUsuario='$user' AND contraseña = '$pass'";
+    //ejecutar la consulta
+    $resultados= mysqli_query($con, $consulta) or die (mysqli_connect_errno ());
+    //alamacenar los datos en un arreglo
+    $fila = mysqli_fetch_array($resultados);
+    //controlar si llegan datos desde la consulta
+    if($fila['id']==null){
+        //redirigir al login
+        header("location:indexLogin.html");
+    }else{
+        //crear la sesion
+        SESSION_START();
+        //definir las variables de sesion y redirigimos a una pagina de usuario
+        $_SESSION['id']= $fila['id'];
+        $_SESSION['nombre']= $fila['nombreUsuario'];
+        header("location:inicioAdmin.php");
+    }
+
+}else{
+    header("location:indexLogin.html");
+}
+?>
+
