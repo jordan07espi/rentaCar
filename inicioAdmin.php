50fe822466ff2a23b384cdbf8bf4f32f5f7b693d
pluis9 <luispalcivar@gmail.com>
2022-12-01 22:53:28 -0500

    modal de eliminacion

--- inicioAdmin.php
+++ inicioAdmin.php
@@ -10,17 +10,20 @@ require_once 'controlador/inicioAdminControl.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/navbar.css">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <title>Bienvenida admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Bienvenida admin</title>
     <!-- Option 1: Include in HTML -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
+
 <body>
     <div>
         <nav class="navbar navbar-expand navbar-dark" id="navbar">
@@ -29,10 +32,11 @@ require_once 'controlador/inicioAdminControl.php';
                     <img src="./img/collab.png" alt="" width="100" height="50">
                     <ul class="nav nav-pills">
                         <li class="nav-item">
-                            <a class="nav-link active; text-white; fs-5" aria-current="page" href="inicioAdmin.php" id="menu">Clientes</a>
+                            <a class="nav-link active; text-white; fs-5" aria-current="page" href="inicioAdmin.php"
+                                id="menu">Clientes</a>
                         </li>
                         <?php $renderOption=$_SESSION['nombreRol'];  echo $renderOption; ?>
-                        
+
                         <li class="nav-item">
                             <a class="nav-link active; text-white; fs-5" href="alquilerAdmin.php" id="menu">Alquiler</a>
                         </li>
@@ -42,11 +46,12 @@ require_once 'controlador/inicioAdminControl.php';
                     </ul>
                     <ul class="nav nav-pills">
                         <li class="nav-item dropdown; position-absolute top-0 end-0" id="botonBien">
-                                <a class="nav-link dropdown-toggle; fs-5" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false" id="menu">
-                                    Bienvenido <?php echo " ".$_SESSION['nombreUsuario']; ?></a>
-                                <ul class="dropdown-menu">
+                            <a class="nav-link dropdown-toggle; fs-5" data-bs-toggle="dropdown" href="#" role="button"
+                                aria-expanded="false" id="menu">
+                                Bienvenido <?php echo " ".$_SESSION['nombreUsuario']; ?></a>
+                            <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="cerrarSesion.php">Cerrar Sesión</a></li>
-                                </ul>
+                            </ul>
                         </li>
                     </ul>
                 </div>
@@ -61,15 +66,15 @@ require_once 'controlador/inicioAdminControl.php';
     <div id="bordeTable">
         <table class="table table-striped">
             <thead class="text-light" id="tabla">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Cedula</th>
-                <th scope="col">Dirección</th>
-                <th scope="col">Teléfono</th>
-               
-                <th scope="col">Acciones</th>
-            </tr>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Cedula</th>
+                    <th scope="col">Dirección</th>
+                    <th scope="col">Teléfono</th>
+
+                    <th scope="col">Acciones</th>
+                </tr>
             </thead>
             <tbody>
                 <?php
@@ -90,11 +95,30 @@ require_once 'controlador/inicioAdminControl.php';
                         <i class="bi bi-pencil-square" ></i>
                         </button>
                         </a>';
-                        echo '<a href="./controlador/clientes/eliminarCliente.php?id=' . $row['idCliente'] . '"> 
-                        <button type="button" class="btn btn-danger">
+                        echo '<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal'.$row['idCliente'].'">
                         <i class="bi bi-trash-fill"></i>
-                        </button>
-                        </a>';
+                  </button>
+                  
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModal'.$row['idCliente'].'" tabindex="-1" aria-labelledby="exampleModalLabel'.$row['idCliente'].'" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5 text-dark" id="exampleModalLabel'.$row['idCliente'].'">Confirmación</h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          <p class="text-dark">¿Está seguro que desea eliminar el registro?</p>
+                        </div>
+                        <div class="modal-footer">
+                          
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><a class="btn btn-info"  
+                          href="./controlador/clientes/eliminarCliente.php?id=' . $row['idCliente'] . '"> Aceptar</a></button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>';
+                       
                         echo '</td>';
                         echo '</tr>';
                         }
@@ -104,13 +128,16 @@ require_once 'controlador/inicioAdminControl.php';
                     }
                     
                 ?>
-        </tbody>
+            </tbody>
         </table>
 
 
 
 
-    <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+        </script>
 </body>
+
 </html>
